26f83e9492e733a52dc1aa30f4c65b71a6a007da
mizuki-y <mizuki-y@syati.info>
2025-08-29 11:56:04 +0900

    Add StructuredParams module with type registration helpers

---
+++ lib/structured_params.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'active_model'
+require 'active_model/type'
+require 'action_controller/metal/strong_parameters'
+
+# version
+require_relative 'structured_params/version'
+
+# types (load first for module definition)
+require_relative 'structured_params/type/object'
+require_relative 'structured_params/type/array'
+
+# params (load after type definitions)
+require_relative 'structured_params/params'
+
+# Main module
+module StructuredParams
+  # Helper method to register types
+  def self.register_types
+    ActiveModel::Type.register(:nested, StructuredParams::Type::Object)
+    ActiveModel::Type.register(:array, StructuredParams::Type::Array)
+  end
+
+  # Helper method to register types with custom names
+  def self.register_types_as(nested_name: :nested, array_name: :array)
+    ActiveModel::Type.register(nested_name, StructuredParams::Type::Object)
+    ActiveModel::Type.register(array_name, StructuredParams::Type::Array)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,3 +14,5 @@ RSpec.configure do |config|
     c.syntax = :expect
   end
 end
+
+StructuredParams.register_types
