4bddc16bc2a5dbaf3f1eff99c52778c7a6f475e4
mizuki-y <mizuki-y@syati.info>
2026-04-03 01:50:09 +0900

    Add operational guide for agents working with structured_params

---
+++ lib/structured_params/attribute_methods.rb
@@ -0,0 +1,29 @@
+# rbs_inline: enabled
+# frozen_string_literal: true
+
+module StructuredParams
+  # Extends ActiveModel::Attributes to define +attr_before_type_cast+ accessors
+  # for each attribute, mirroring ActiveRecord::AttributeMethods::BeforeTypeCast.
+  #
+  # Example:
+  #   class UserParams < StructuredParams::Params
+  #     attribute :age, :integer
+  #   end
+  #
+  #   params = UserParams.new(age: "42abc")
+  #   params.age                    # => 42  (type-cast)
+  #   params.age_before_type_cast   # => "42abc"  (raw input)
+  module AttributeMethods
+    extend ActiveSupport::Concern
+
+    included do
+      # Override attribute to also define `attr_before_type_cast`
+      # via ActiveModel::Attribute#value_before_type_cast
+      #: (String | Symbol name, *untyped) -> void
+      def self.attribute(name, ...)
+        super
+        define_method(:"#{name}_before_type_cast") { @attributes[name.to_s].value_before_type_cast }
+      end
+    end
+  end
+end
